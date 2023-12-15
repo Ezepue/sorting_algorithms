de375e0f0b171c2c93ca732d8a3e1181e48eca51
Ezepue <ebukaezepue@gmail.com>
2023-12-15 13:48:56 +0100

    Test 4

--- sort.h
+++ sort.h
@@ -24,7 +24,14 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+
+
 void insertion_sort_list(listint_t **list);
+void insert_node_at_correct_position(listint_t **list, listint_t *node);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+
+
+
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void helper_func(int *arr, int start, int end, size_t len);
